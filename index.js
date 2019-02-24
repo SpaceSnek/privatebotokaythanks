fc6a0b3650928959dedaee38374d1aa8d0345fb7
SpaceSnek <47842300+SpaceSnek@users.noreply.github.com>
2019-02-24 00:52:12 -0500

    Update index.js

--- index.js
+++ index.js
@@ -32,6 +32,16 @@ bot.on('message', message => {
 });
 
 //This Section dms the users the Help
+//bot.on('message', message => {
+ // if (message.content === '.help') {
+  //  message.delete();
+  //  message.author.send("--Commands List--")
+  //  message.author.send("`.motd = PM's User a link to the PRP MOTD`") 
+  //  message.author.send("`.forums = PM's User a link to the PRP FORUMS`")
+  }
+});
+
+//This section Controls all Commands issued to the bot
 bot.on('message', message => {
   if (message.content === '.help') {
     message.delete();
@@ -39,27 +49,54 @@ bot.on('message', message => {
     message.author.send("`.motd = PM's User a link to the PRP MOTD`") 
     message.author.send("`.forums = PM's User a link to the PRP FORUMS`")
   }
-});
-
-//This section dms the users a link to the forums
-bot.on('message', message => {
   if (message.content === '.forums') {
     message.delete();
     message.author.send('https://forums.prpservers.com/')
   }
-});
-
-//This Section dms the users the MOTD
-bot.on('message', message => {
   if (message.content === '.motd') {
     message.delete();
     message.author.send('https://forums.prpservers.com/threads/drp-rules.4/')
   }
+  if (message.content === '.donate') {
+    message.delete();
+    message.author.send('https://donate.prpservers.com/')
+  }
+  if (message.content === '.steam') {
+    message.delete();
+    message.author.send('https://steamcommunity.com/groups/prpservers')
+  }
 });
+
+//This Section dms the users the MOTD
+//bot.on('message', message => {
+ // if (message.content === '.motd') {
+   // message.delete();
+  //  message.author.send('https://forums.prpservers.com/threads/drp-rules.4/')
+ // }
+//});
+
+//This Section dms the users the donation page
+//bot.on('message', message => {
+ // if (message.content === '.donate') {
+  //  message.delete();
+  //  message.author.send('https://donate.prpservers.com/')
+ // }
+//});
+
+//This Section dms the users the donation page
+//bot.on('message', message => {
+ // if (message.content === '.steam') {
+   // message.delete();
+  //  message.author.send('https://steamcommunity.com/groups/prpservers')
+//  }
+//});
 
 //This section Dms new users and tell thems to read the rules
 bot.on('guildMemberAdd', member => {
   member.send("Welcome to the server! Please read the section titled rules before proceeding! By joining the server you agree to adhere to the rules.");
 });
 
+//How tall is hyper active?
+if (message.content === 'how tall is hyper?') {
+  msg.channel.send("Hyperactive is" + Math.floor(Math.Random() * 5 + 1) + "foot" + Math.floor(Math.random() *12 +1)
 bot.login(process.env.BOT_TOKEN);
